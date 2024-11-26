091022ab0b2039b820608517775d15bef3d18e58
Btu2023V <119510270+Btu2023V@users.noreply.github.com>
2024-11-26 20:23:00 +0800

    Update aria2.sh

--- aria2.sh
+++ aria2.sh
@@ -115,7 +115,7 @@ Download_aria2() {
         echo -e "${Info} 删除旧版 Aria2 二进制文件..."
         rm -vf $(which aria2c)
     done
-    DOWNLOAD_URL="https://github.com/Btu2023V/Aria2-Pro-Core/releases/download/${aria2_new_ver}/aria2-${aria2_new_ver%_*}-static-linux-${ARCH}.tar.gz"
+    DOWNLOAD_URL="https://github.com/Btu2023V/Aria2-Pro-Core/releases/download/${aria2_new_ver}/aria2.${aria2_new_ver%_*}-static-linux-${ARCH}.tar.gz"
     {
         wget -t2 -T3 -O- "${DOWNLOAD_URL}"
     } | tar -zx
